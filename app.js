a22499f96414d94c3376fbed2126f9a33b999774
Nguyen <ngyale@3c0630478c7e.ant.amazon.com>
2022-02-15 02:43:23 +0100

    added POST addDog

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const cors = require("cors");
 
 const dogsRouter = require("./routers/dogsRouter");
+const addDogRouter = require("./routers/addDogRouter");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -10,6 +11,7 @@ app.use(cors());
 app.use(express.json());
 
 app.use("/dogs", dogsRouter);
+app.use("/addDog", addDogRouter);
 
 app.get("/", (req, res) => {
     res.send("<h1>Hello from Express!</h1>");
